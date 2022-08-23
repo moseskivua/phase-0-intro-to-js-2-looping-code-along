9397a31ee920459be2f398b7c6e235a0d1ba3ca9
Moses Kivua <kivuamoses9@gmail.com>
2022-08-23 15:45:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const cardContent=[];
+    for (let i = 0; i<names.length; i++) {
+        cardContent.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`,);
+    }
+    return cardContent
+}
+function countDown() {
+    let countdown = 10 ;
+    while (countdown >= 0) {
+    console.log(countdown--);
+}
+}
